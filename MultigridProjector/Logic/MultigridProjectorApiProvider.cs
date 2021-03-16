366d7a39f05a1de66de8e0bd3378485ab7ac61bb
Viktor Ferenczi <viktor@ferenczi.eu>
2021-03-17 00:41:06 +0100

    0.1.19

--- MultigridProjector/Logic/MultigridProjectorApiProvider.cs
+++ MultigridProjector/Logic/MultigridProjectorApiProvider.cs
@@ -13,7 +13,7 @@ namespace MultigridProjector.Logic
         private static MultigridProjectorApiProvider _instance;
         public static IMultigridProjectorApi Instance => _instance ?? (_instance = new MultigridProjectorApiProvider());
 
-        public string Version => "0.1.18";
+        public string Version => "0.1.19";
 
         public int GetSubgridCount(long projectorId)
         {
