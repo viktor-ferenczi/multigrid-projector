a45e912c7e60372399b51fa0688001a1ae32e6e9
Viktor Ferenczi <viktor@ferenczi.eu>
2021-03-21 22:27:31 +0100

    Test Mod logging fixes

--- MultigridProjectorClient/Mod/Data/Scripts/MultigridProjectorMod/MultigridProjectorMod.cs
+++ MultigridProjectorClient/Mod/Data/Scripts/MultigridProjectorMod/MultigridProjectorMod.cs
@@ -28,14 +28,17 @@ namespace MultigridProjectorMod
             _projector = Entity as IMyProjector;
             if (_projector == null)
             {
-                MyLog.Default.WriteLine($"MultigridProjectorMod: No projector");
+                MyLog.Default.WriteLineAndConsole($"MultigridProjectorMod: No projector");
                 return;
             }
 
-            if (_projector.Closed || !_projector.InScene || _projector.OwnerId == 0)
+            if (_projector.Closed)
+            {
+                MyLog.Default.WriteLineAndConsole($"MultigridProjectorMod: Projector {_projector.DisplayName} [{_projector.EntityId}] skipped because it is closed");
                 return;
+            }
 
-            MyLog.Default.WriteLine($"MultigridProjectorMod: Projector {_projector.DisplayName} [{_projector.EntityId}] registered");
+            MyLog.Default.WriteLineAndConsole($"MultigridProjectorMod: Projector {_projector.DisplayName} [{_projector.EntityId}] registered");
 
             _mgp = new MultigridProjectorModAgent();
 
@@ -47,7 +50,7 @@ namespace MultigridProjectorMod
             if(_projector == null)
                 return;
 
-            MyLog.Default.WriteLine($"MultigridProjectorMod: Projector {_projector.DisplayName} [{_projector.EntityId}] closed");
+            MyLog.Default.WriteLineAndConsole($"MultigridProjectorMod: Projector {_projector.DisplayName} [{_projector.EntityId}] closed");
 
             _mgp = null;
             
@@ -79,75 +82,75 @@ namespace MultigridProjectorMod
 
         private void LogProjector(long projectorEntityId)
         {
-            MyLog.Default.WriteLine($"MultigridProjectorMod: Projector {_projector.DisplayName} [{projectorEntityId}] MGP API connection is available");
+            MyLog.Default.WriteLineAndConsole($"MultigridProjectorMod: Projector {_projector.DisplayName} [{projectorEntityId}] MGP API connection is available");
             MyAPIGateway.Utilities.ShowMessage("MGP", $"Client plugin version {_mgp.Version}");
             MyAPIGateway.Utilities.ShowMessage("MGP", $"Projector {_projector.DisplayName} [{projectorEntityId}] is usable with multigrid blueprints!");
         }
 
         private void LogBlueprintDetails(long projectorEntityId, List<MyObjectBuilder_CubeGrid> gridBuilders)
         {
-            MyLog.Default.WriteLine("==================================================================");
-            MyLog.Default.WriteLine($"MultigridProjectorMod: Projector {_projector.DisplayName} [{projectorEntityId}]:");
-            MyLog.Default.WriteLine("==================================================================");
-            MyLog.Default.WriteLine($"Projector's grid ID: {_projector.CubeGrid?.EntityId}");
+            MyLog.Default.WriteLineAndConsole("==================================================================");
+            MyLog.Default.WriteLineAndConsole($"MultigridProjectorMod: Projector {_projector.DisplayName} [{projectorEntityId}]:");
+            MyLog.Default.WriteLineAndConsole("==================================================================");
+            MyLog.Default.WriteLineAndConsole($"Projector's grid ID: {_projector.CubeGrid?.EntityId}");
 
             if (gridBuilders == null)
             {
-                MyLog.Default.WriteLine($"No blueprint loaded.");
-                MyLog.Default.WriteLine("==================================================================");
+                MyLog.Default.WriteLineAndConsole($"No blueprint loaded.");
+                MyLog.Default.WriteLineAndConsole("==================================================================");
                 return;
             }
 
-            MyLog.Default.WriteLine($"Blueprint loaded:");
+            MyLog.Default.WriteLineAndConsole($"Blueprint loaded:");
             
             var subgridCount = _mgp.GetSubgridCount(projectorEntityId);
-            MyLog.Default.WriteLine($"Subgrid count: {subgridCount}");
-            MyLog.Default.WriteLine($"Subgrids in blueprint: {gridBuilders.Count}");
+            MyLog.Default.WriteLineAndConsole($"Subgrid count: {subgridCount}");
+            MyLog.Default.WriteLineAndConsole($"Subgrids in blueprint: {gridBuilders.Count}");
 
             for (var subgridIndex = 0; subgridIndex < subgridCount; subgridIndex++)
             {
-                MyLog.Default.WriteLine("-------------------------");
-                MyLog.Default.WriteLine($"Subgrid #{subgridIndex}");
-                MyLog.Default.WriteLine("-------------------------");
+                MyLog.Default.WriteLineAndConsole("-------------------------");
+                MyLog.Default.WriteLineAndConsole($"Subgrid #{subgridIndex}");
+                MyLog.Default.WriteLineAndConsole("-------------------------");
 
                 var previewGrid = _mgp.GetPreviewGrid(projectorEntityId, 0);
-                MyLog.Default.WriteLine($"Preview grid ID: {previewGrid?.EntityId}");
+                MyLog.Default.WriteLineAndConsole($"Preview grid ID: {previewGrid?.EntityId}");
 
                 var builtGrid = _mgp.GetBuiltGrid(projectorEntityId, 0);
-                MyLog.Default.WriteLine(builtGrid == null ? "No built grid for this subgrid" : $"Built grid ID: {builtGrid.EntityId}");
+                MyLog.Default.WriteLineAndConsole(builtGrid == null ? "No built grid for this subgrid" : $"Built grid ID: {builtGrid.EntityId}");
 
-                MyLog.Default.WriteLine("---");
+                MyLog.Default.WriteLineAndConsole("---");
 
                 var blockStates = new Dictionary<Vector3I, BlockState>();
                 _mgp.GetBlockStates(blockStates, projectorEntityId, 0, new BoundingBoxI(Vector3I.MinValue, Vector3I.MaxValue), ~0);
 
                 if (blockStates.Count > 0)
-                    MyLog.Default.WriteLine($"First block state: {_mgp.GetBlockState(projectorEntityId, 0, blockStates.Keys.First())}");
+                    MyLog.Default.WriteLineAndConsole($"First block state: {_mgp.GetBlockState(projectorEntityId, 0, blockStates.Keys.First())}");
 
-                MyLog.Default.WriteLine($"Block states:");
+                MyLog.Default.WriteLineAndConsole($"Block states:");
                 foreach (var pair in blockStates)
                 {
-                    MyLog.Default.WriteLine($"  {pair.Key} => {pair.Value}");
+                    MyLog.Default.WriteLineAndConsole($"  {pair.Key} => {pair.Value}");
                 }
 
-                MyLog.Default.WriteLine("---");
+                MyLog.Default.WriteLineAndConsole("---");
 
-                MyLog.Default.WriteLine($"Base connections:");
+                MyLog.Default.WriteLineAndConsole($"Base connections:");
                 foreach (var pair in _mgp.GetBaseConnections(projectorEntityId, 0))
                 {
-                    MyLog.Default.WriteLine($"  {pair.Key} => #{pair.Value.GridIndex} @ {pair.Value.Position}");
+                    MyLog.Default.WriteLineAndConsole($"  {pair.Key} => #{pair.Value.GridIndex} @ {pair.Value.Position}");
                 }
 
-                MyLog.Default.WriteLine("---");
+                MyLog.Default.WriteLineAndConsole("---");
 
-                MyLog.Default.WriteLine($"Base connections:");
+                MyLog.Default.WriteLineAndConsole($"Base connections:");
                 foreach (var pair in _mgp.GetTopConnections(projectorEntityId, 0))
                 {
-                    MyLog.Default.WriteLine($"  {pair.Key} => #{pair.Value.GridIndex} @ {pair.Value.Position}");
+                    MyLog.Default.WriteLineAndConsole($"  {pair.Key} => #{pair.Value.GridIndex} @ {pair.Value.Position}");
                 }
             }
 
-            MyLog.Default.WriteLine("==================================================================");
+            MyLog.Default.WriteLineAndConsole("==================================================================");
         }
     }
 }
