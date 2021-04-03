70d3a4559ff82753b3225e76fb3d6d9c5facd676
Viktor Ferenczi <viktor@ferenczi.eu>
2021-04-03 05:18:58 +0200

    Fixed console block visuals

--- MultigridProjectorClient/Patches/MyProjectorBase_UpdateProjection.cs
+++ MultigridProjectorClient/Patches/MyProjectorBase_UpdateProjection.cs
@@ -15,8 +15,14 @@ namespace MultigridProjector.Patches
         // ReSharper disable once UnusedMember.Local
         private static bool Prefix(MyProjectorBase __instance)
         {
+            var projector = __instance;
+
+            // Console blocks set up the hologram look here
+            if (!projector.AllowWelding || projector.AllowScaling)
+                return true;
+
             // Disallow any use of the original ProjectorUpdateWork
-            // In theory this is not called at all, just in case.
+            // In theory this is not called for projectors, but disable it just in case.
             return false;
         }
     }
