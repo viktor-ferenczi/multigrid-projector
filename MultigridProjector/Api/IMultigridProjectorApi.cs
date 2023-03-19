0aea79eb151ce7672bf7d0d02110d19b4fe2dd28
Viktor Ferenczi <viktor@ferenczi.eu>
2023-03-19 22:26:18 +0100

    0.4.15

--- MultigridProjector/Api/IMultigridProjectorApi.cs
+++ MultigridProjector/Api/IMultigridProjectorApi.cs
@@ -7,7 +7,7 @@ namespace MultigridProjector.Api
 {
     public interface IMultigridProjectorApi
     {
-        // Multigrid Projector version: 0.4.14
+        // Multigrid Projector version: 0.4.15
         string Version { get; }
 
         // Returns the number of subgrids in the active projection, returns zero if there is no projection
