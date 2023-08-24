5da6d33e9c24341da7e672cc28345ded434a5c57
Viktor Ferenczi <viktor@ferenczi.eu>
2023-08-24 22:01:25 +0200

    0.5.2

--- MultigridProjector/Api/IMultigridProjectorApi.cs
+++ MultigridProjector/Api/IMultigridProjectorApi.cs
@@ -7,7 +7,7 @@ namespace MultigridProjector.Api
 {
     public interface IMultigridProjectorApi
     {
-        // Multigrid Projector version: 0.5.1
+        // Multigrid Projector version: 0.5.2
         string Version { get; }
 
         // Returns the number of subgrids in the active projection, returns zero if there is no projection
--- MultigridProjectorMods/ApiTest/metadata.mod
+++ MultigridProjectorMods/ApiTest/metadata.mod
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="utf-8"?>
 <ModMetadata xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <ModVersion>0.5.1</ModVersion>
+  <ModVersion>0.5.2</ModVersion>
 </ModMetadata>
