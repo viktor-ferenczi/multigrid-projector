f8e06fd75881532af1af370137deb67d97aa82a8
Space <unclearspace@gmail.com>
2023-09-24 10:37:04 +0100

    Allow sorting by column

--- MultigridProjectorClient/Menus/CraftDialog.cs
+++ MultigridProjectorClient/Menus/CraftDialog.cs
@@ -1,4 +1,5 @@
 ﻿using MultigridProjectorClient.Utilities;
+using Sandbox;
 using Sandbox.Graphics.GUI;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using VRage.Utils;
 using VRageMath;
+using static Sandbox.Graphics.GUI.MyGuiControlTable;
 
 namespace MultigridProjectorClient.Menus
 {
@@ -15,7 +17,7 @@ namespace MultigridProjectorClient.Menus
 
         public static MyGuiScreenMessageBox CreateDialog(
             string assemblerName,
-            HashSet<MyGuiControlTable.Row> rows,
+            HashSet<Row> rows,
             Action assembleAll = null,
             Action assembleMissing = null,
             Action onClosing = null)
@@ -88,17 +90,99 @@ namespace MultigridProjectorClient.Menus
             componentTable.SetColumnName(2, new StringBuilder("Inventory"));
             componentTable.SetColumnName(3, new StringBuilder("Blueprint"));
 
-            // Order rows by missing components (cell index 1)
-            List<MyGuiControlTable.Row> orderedRows = rows.OrderByDescending(x => int.Parse(x.GetCell(3).Text.ToString().Replace(",", ""))).ToList();
-            for (int i = 0; i < orderedRows.Count; i++)
+            componentTable.ColumnClicked += (table, column) =>
             {
-                componentTable.Insert(i, orderedRows[i]);
+                // If the column is > 0 it contains a number which we sort by descending order
+                // Otherwise we sort by ascending (alphabetical) order
+                bool invert = column > 0;
+
+                // Invert sorting if the column is already sorted
+                List<Row> rowsCopy = new List<Row>(table.Rows);
+                SortRows(ref rowsCopy, column, invert);
+
+                if (table.Rows.SequenceEqual(rowsCopy))
+                {
+                    SortByColumn(table, column, !invert);
+                }
+                else
+                {
+                    SortByColumn(table, column, invert);
+                }
+
+                // The click sound is not played so we play it ourself
+                MyGuiSoundManager.PlaySound(VRage.Audio.GuiSounds.MouseClick);
+            };
+
+            foreach (Row row in rows)
+            {
+                componentTable.Add(row);
             }
+
+            SortByColumn(componentTable, 3, true);
 
             MyGuiControls controls = (MyGuiControls)Reflection.GetValue(typeof(MyGuiScreenBase), messageBox, "m_controls");
             controls.Add(componentTable);
 
             return messageBox;
+        }
+        private static string GetCellText(Row row, int column)
+        {
+            return row.GetCell(column).Text.ToString();
+        }
+
+        private static void SortRows(ref List<Row> rows, int column, bool inverse = false)
+        {
+            rows.Sort((row1, row2) =>
+            {
+                string cell1 = GetCellText(row1, column);
+                string cell2 = GetCellText(row2, column);
+
+                // Numerical Order
+                if (int.TryParse(cell1.Replace(",", ""), out int value1)
+                    && int.TryParse(cell2.Replace(",", ""), out int value2))
+                {
+                    // Sort by name if values match
+                    if (value1 == value2)
+                    {
+                        string name1 = GetCellText(row1, 0);
+                        string name2 = GetCellText(row2, 0);
+
+                        if (inverse)
+                        {
+                            return name2.CompareTo(name1);
+                        }
+
+                        return name1.CompareTo(name2);
+                    }
+
+                    return value1.CompareTo(value2);
+                }
+
+                // Alphabetical Order
+                return cell1.CompareTo(cell2);
+            });
+
+            if (inverse)
+            {
+                rows.Reverse();
+            }
+        }
+
+        private static void SortByColumn(MyGuiControlTable table, int column, bool inverse = false)
+        {
+            List<Row> rows = new List<Row>(table.Rows);
+
+            foreach (Row row in rows)
+            {
+                table.Remove(row);
+            }
+
+            SortRows(ref rows, column, inverse);
+
+            for (int i = 0; i < rows.Count; i++)
+            {
+                table.Insert(i, rows[i]);
+            }
         }
     }
 }
