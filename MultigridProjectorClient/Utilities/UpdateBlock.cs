01cc00b368c8eee144f1bf949dd24b50b6f75bdc
Viktor Ferenczi <viktor@ferenczi.eu>
2024-09-08 00:19:01 +0200

    Refactoring (no functional change)

--- MultigridProjectorClient/Utilities/UpdateBlock.cs
+++ MultigridProjectorClient/Utilities/UpdateBlock.cs
@@ -21,16 +21,14 @@ using IMyEventControllerBlock = Sandbox.ModAPI.Ingame.IMyEventControllerBlock;
 using SpaceEngineers.Game.EntityComponents.Blocks;
 using VRage.Sync;
 using Sandbox.Graphics.GUI;
-using VRage.ObjectBuilder;
 using MultigridProjector.Logic;
-using VRage.Game.Entity;
 
 namespace MultigridProjectorClient.Utilities
 {
     internal static class UpdateToolbar
     {
-        private const string UnknownText = "UNKNOWN ACTION";
-        private const string PlaceholderText = "ACTION ENTITY NOT FOUND";
+        private const string UNKNOWN_TEXT = "UNKNOWN ACTION";
+        private const string PLACEHOLDER_TEXT = "ACTION ENTITY NOT FOUND";
 
         private static MyToolbarItem CreateTerminalToolbarItem(MyObjectBuilder_ToolbarItemTerminalBlock builder)
         {
@@ -70,7 +68,7 @@ namespace MultigridProjectorClient.Utilities
             MyObjectBuilder_ToolbarItemTerminalGroup data = MyObjectBuilderSerializer.CreateNewObject<MyObjectBuilder_ToolbarItemTerminalGroup>();
             data.GroupName = text;
             data._Action = "";
-            data.Parameters = new List<MyObjectBuilder_ToolbarItemActionParameter> { MyObjectBuilderSerializer.CreateNewObject<MyObjectBuilder_ToolbarItemActionParameter>() };
+            data.Parameters = new List<MyObjectBuilder_ToolbarItemActionParameter> {MyObjectBuilderSerializer.CreateNewObject<MyObjectBuilder_ToolbarItemActionParameter>()};
 
             // This is used internally to find which grid the block group is on.
             // It needs to be set to the block we want to assign the toolbar to.
@@ -81,9 +79,9 @@ namespace MultigridProjectorClient.Utilities
 
         private static MyBlockGroup CreateDummyGroup(string name, MyTerminalBlock dummyBlock)
         {
-            MyBlockGroup dummyGroup = (MyBlockGroup)Activator.CreateInstance(typeof(MyBlockGroup), true);
+            MyBlockGroup dummyGroup = (MyBlockGroup) Activator.CreateInstance(typeof(MyBlockGroup), true);
             dummyGroup.Name = new StringBuilder(name);
-            Reflection.SetValue(dummyGroup, "Blocks", new HashSet<MyTerminalBlock> { dummyBlock });
+            Reflection.SetValue(dummyGroup, "Blocks", new HashSet<MyTerminalBlock> {dummyBlock});
 
             MyGridTerminalSystem terminalSystem = dummyBlock.CubeGrid.GridSystems.TerminalSystem;
             terminalSystem.AddUpdateGroup(dummyGroup, true);
@@ -104,7 +102,7 @@ namespace MultigridProjectorClient.Utilities
             // We can sidestep this by making a group and removing it once the item is added
             // TODO: Use an event rather then a fixed delay
             MyBlockGroup group = CreateDummyGroup(name, dummyBlock);
-            Events.InvokeOnGameThread(() => toolbar.SetItemAtIndex(index, item), 20);   
+            Events.InvokeOnGameThread(() => toolbar.SetItemAtIndex(index, item), 20);
             Events.InvokeOnGameThread(() => RemoveDummyGroup(group), 40);
         }
 
@@ -126,7 +124,7 @@ namespace MultigridProjectorClient.Utilities
                 return flightMovementBlock.Toolbar;
 
             if (block is MyAirVent airVent)
-                return (MyToolbar)Reflection.GetValue(airVent, "m_actionToolbar");
+                return (MyToolbar) Reflection.GetValue(airVent, "m_actionToolbar");
 
             // Cockpits, remote controls and cryopods
             if (block is MyShipController shipController)
@@ -158,8 +156,8 @@ namespace MultigridProjectorClient.Utilities
                     // Make a placeholder if the entity the toolbar is attached to could not be found
                     if (newToolbarItem == null)
                     {
-                        newToolbarItem = CreateDummyToolbarItem(PlaceholderText, destinationBlock.EntityId);
-                        SetItemAtIndexWithDummyGroup(destinationToolbar, i, newToolbarItem, PlaceholderText, destinationBlock);
+                        newToolbarItem = CreateDummyToolbarItem(PLACEHOLDER_TEXT, destinationBlock.EntityId);
+                        SetItemAtIndexWithDummyGroup(destinationToolbar, i, newToolbarItem, PLACEHOLDER_TEXT, destinationBlock);
                         continue;
                     }
 
@@ -190,8 +188,8 @@ namespace MultigridProjectorClient.Utilities
 
                 // If the toolbar item is of an unknown type make a dummy item as an error message
                 {
-                    MyToolbarItem newToolbarItem = CreateDummyToolbarItem(UnknownText, destinationBlock.EntityId);
-                    SetItemAtIndexWithDummyGroup(destinationToolbar, i, newToolbarItem, UnknownText, destinationBlock);
+                    MyToolbarItem newToolbarItem = CreateDummyToolbarItem(UNKNOWN_TEXT, destinationBlock.EntityId);
+                    SetItemAtIndexWithDummyGroup(destinationToolbar, i, newToolbarItem, UNKNOWN_TEXT, destinationBlock);
 
                     PluginLog.Error($"Cannot process toolbar item: {toolbarItem}");
                 }
@@ -201,17 +199,20 @@ namespace MultigridProjectorClient.Utilities
 
     internal static class UpdateBlock
     {
+        private static readonly HashSet<string> ExcludedEventControllerProperties = new HashSet<string> {"SearchBox"};
+
         public static void CopyProperties(MyTerminalBlock sourceBlock, MyTerminalBlock destinationBlock)
         {
             // Special Cases
             if (sourceBlock is MyEventControllerBlock sourceEventControllerBlock &&
                 destinationBlock is MyEventControllerBlock destinationEventControllerBlock)
             {
-                CopyTerminalProperties(sourceBlock, destinationBlock, new HashSet<string> { "SearchBox" });
+                // Copy over terminal properties
+                CopyTerminalProperties(sourceBlock, destinationBlock, ExcludedEventControllerProperties);
                 UpdateToolbar.CopyToolbars(sourceBlock, destinationBlock);
-                CopyEvents(sourceEventControllerBlock, destinationEventControllerBlock);
-                Events.InvokeOnGameThread(() => CopyPowerState(sourceBlock, destinationBlock));
 
+                // Events in Event Controllers are not stored as properties, so copy those as well
+                CopyEvents(sourceEventControllerBlock, destinationEventControllerBlock);
             }
             else
             {
@@ -227,16 +228,17 @@ namespace MultigridProjectorClient.Utilities
                 }
 
                 else if (sourceBlock is IMyProgrammableBlock sourceProgrammableBlock &&
-                    destinationBlock is IMyProgrammableBlock destinationProgrammableBlock &&
-                    MySession.Static.IsSettingsExperimental())
+                         destinationBlock is IMyProgrammableBlock destinationProgrammableBlock &&
+                         MySession.Static.IsSettingsExperimental())
                 {
                     CopyScripts(sourceProgrammableBlock, destinationProgrammableBlock);
                 }
 
                 // Copying power must be done in the next frame as disabling a block will prevent properties being modified
                 // so we need to wait for all the changes to process
-                Events.InvokeOnGameThread(() => CopyPowerState(sourceBlock, destinationBlock));
             }
+
+            Events.InvokeOnGameThread(() => CopyPowerState(sourceBlock, destinationBlock));
         }
 
         // TODO: Refactor into multiple functions
@@ -245,81 +247,84 @@ namespace MultigridProjectorClient.Utilities
         // See if this can be moved out of the special cases
         private static void CopyEvents(MyEventControllerBlock sourceBlock, MyEventControllerBlock destinationBlock)
         {
-
             Events.InvokeOnGameThread(() =>
             {
-                long eventId = ((Sandbox.ModAPI.IMyEventControllerBlock)sourceBlock).SelectedEvent.UniqueSelectionId;
-                Delegate SelectEvent = Reflection.GetMethod(typeof(MyEventControllerBlock), destinationBlock, "SelectEvent");
-                SelectEvent.DynamicInvoke(eventId);
+                long eventId = ((Sandbox.ModAPI.IMyEventControllerBlock) sourceBlock).SelectedEvent.UniqueSelectionId;
+                Delegate selectEvent = Reflection.GetMethod(typeof(MyEventControllerBlock), destinationBlock, "SelectEvent");
+                selectEvent.DynamicInvoke(eventId);
             }, 100);
 
-            Events.InvokeOnGameThread(() =>
+            Events.InvokeOnGameThread(() => { CopyEventControllerCondition(sourceBlock, destinationBlock); }, 200);
+
+            Events.InvokeOnGameThread(() => { CopyEventControllerBlockSelection(sourceBlock, destinationBlock); }, 300);
+        }
+        private static void CopyEventControllerCondition(MyEventControllerBlock sourceBlock, MyEventControllerBlock destinationBlock)
+        {
+
+            ((IMyEventControllerBlock) destinationBlock).Threshold = ((IMyEventControllerBlock) sourceBlock).Threshold;
+            ((IMyEventControllerBlock) destinationBlock).IsLowerOrEqualCondition = ((IMyEventControllerBlock) sourceBlock).IsLowerOrEqualCondition;
+            ((IMyEventControllerBlock) destinationBlock).IsAndModeEnabled = ((IMyEventControllerBlock) sourceBlock).IsAndModeEnabled;
+
+            if (sourceBlock.Components.TryGet<MyEventAngleChanged>(out var sourceComp) &&
+                destinationBlock.Components.TryGet<MyEventAngleChanged>(out var destinationComp))
             {
-                ((IMyEventControllerBlock)destinationBlock).Threshold = ((IMyEventControllerBlock)sourceBlock).Threshold;
-                ((IMyEventControllerBlock)destinationBlock).IsLowerOrEqualCondition = ((IMyEventControllerBlock)sourceBlock).IsLowerOrEqualCondition;
-                ((IMyEventControllerBlock)destinationBlock).IsAndModeEnabled = ((IMyEventControllerBlock)sourceBlock).IsAndModeEnabled;
+                Sync<float, SyncDirection.BothWays> sourceAngle = (Sync<float, SyncDirection.BothWays>) Reflection.GetValue(sourceComp, "m_angle");
+                Sync<float, SyncDirection.BothWays> destinationAngle = (Sync<float, SyncDirection.BothWays>) Reflection.GetValue(destinationComp, "m_angle");
 
-                if (sourceBlock.Components.TryGet<MyEventAngleChanged>(out var sourceComp) &&
-                    destinationBlock.Components.TryGet<MyEventAngleChanged>(out var destinationComp))
-                {
-                    Sync<float, SyncDirection.BothWays> sourceAngle = (Sync<float, SyncDirection.BothWays>)Reflection.GetValue(sourceComp, "m_angle");
-                    Sync<float, SyncDirection.BothWays> destinationAngle = (Sync<float, SyncDirection.BothWays>)Reflection.GetValue(destinationComp, "m_angle");
-                    
-                    destinationAngle.Value = sourceAngle.Value;
-                }
-                else
-                {
-                    PluginLog.Error($"Could not find angle for block: {sourceBlock.DisplayName}");
-                }
-            }, 200);
-
-            Events.InvokeOnGameThread(() =>
+                destinationAngle.Value = sourceAngle.Value;
+            }
+            else
             {
-                MultigridProjection.TryFindProjectionByProjector(sourceBlock.CubeGrid.Projector, out MultigridProjection projection);
-                var (foundIds, missingIds) = projection.GetSelectedBlockIdsFromEventController(destinationBlock);
+                PluginLog.Error($"Could not find angle for block: {sourceBlock.DisplayName}");
+            }
+        }
+        private static void CopyEventControllerBlockSelection(MyEventControllerBlock sourceBlock, MyEventControllerBlock destinationBlock)
+        {
 
-                // SelectAvailableBlocks and SelectButton expect MyGuiControlListbox.Item
-                List<MyGuiControlListbox.Item> foundBlocks = new List<MyGuiControlListbox.Item>();
-                foreach (long blockId in foundIds)
+            MultigridProjection.TryFindProjectionByProjector(sourceBlock.CubeGrid.Projector, out MultigridProjection projection);
+            var (foundIds, missingIds) = projection.GetSelectedBlockIdsFromEventController(destinationBlock);
+
+            // SelectAvailableBlocks and SelectButton expect MyGuiControlListbox.Item
+            List<MyGuiControlListbox.Item> foundBlocks = new List<MyGuiControlListbox.Item>();
+            foreach (long blockId in foundIds)
+            {
+                foundBlocks.Add(new MyGuiControlListbox.Item(userData: blockId));
+            }
+
+            List<MyCubeBlock> missingBlocks = new List<MyCubeBlock>();
+            foreach (long blockId in missingIds)
+            {
+                missingBlocks.Add((MyCubeBlock) MyEntities.GetEntityById(blockId));
+            }
+
+
+            if (foundBlocks.Count > 0)
+            {
+                Delegate selectAvailableBlocks = Reflection.GetMethod(typeof(MyEventControllerBlock), destinationBlock, "SelectAvailableBlocks");
+                selectAvailableBlocks.DynamicInvoke(foundBlocks);
+
+                Delegate selectButton = Reflection.GetMethod(typeof(MyEventControllerBlock), destinationBlock, "SelectButton");
+                selectButton.DynamicInvoke();
+            }
+
+            if (missingBlocks.Count > 0)
+            {
+                StringBuilder message = new StringBuilder("The following blocks could not be found:\n");
+
+                foreach (var block in missingBlocks)
                 {
-                    foundBlocks.Add(new MyGuiControlListbox.Item(userData: blockId));
+                    message.AppendLine($"{block.DisplayName}");
                 }
 
-                List<MyCubeBlock> missingBlocks = new List<MyCubeBlock>();
-                foreach (long blockId in missingIds)
-                {
-                    missingBlocks.Add((MyCubeBlock)MyEntities.GetEntityById(blockId));
-                }
-
-
-                if (foundBlocks.Count > 0)
-                {
-                    Delegate SelectAvailableBlocks = Reflection.GetMethod(typeof(MyEventControllerBlock), destinationBlock, "SelectAvailableBlocks");
-                    SelectAvailableBlocks.DynamicInvoke(foundBlocks);
-
-                    Delegate SelectButton = Reflection.GetMethod(typeof(MyEventControllerBlock), destinationBlock, "SelectButton");
-                    SelectButton.DynamicInvoke();
-                }
-
-                if (missingBlocks.Count > 0)
-                {
-                    StringBuilder message = new StringBuilder("The following blocks could not be found:\n");
-
-                    foreach (var block in missingBlocks)
-                    {
-                        message.AppendLine($"{block.DisplayName}");
-                    }
-
-                    MyAPIGateway.Utilities.ShowMessage("Multigrid Projector", message.ToString());
-                }
-            }, 300);
+                MyAPIGateway.Utilities.ShowMessage("Multigrid Projector", message.ToString());
+            }
         }
 
         private static void CopyPowerState(MyTerminalBlock sourceBlock, MyTerminalBlock destinationBlock)
         {
             if (destinationBlock.GetProperty("OnOff") == null)
                 return;
-            
+
             destinationBlock.SetValue("OnOff", sourceBlock.GetValue<bool>("OnOff"));
         }
 
@@ -373,13 +378,13 @@ namespace MultigridProjectorClient.Utilities
 
         private static void CopyBlueprints(MyProjectorBase sourceBlock, MyProjectorBase destinationBlock)
         {
-            List<MyObjectBuilder_CubeGrid> projectedGrids = (List<MyObjectBuilder_CubeGrid>)Reflection.GetValue(sourceBlock, "m_savedProjections");
-            Delegate InitFromObjectBuilder = Reflection.GetMethod(typeof(MyProjectorBase), destinationBlock, "InitFromObjectBuilder");
+            List<MyObjectBuilder_CubeGrid> projectedGrids = (List<MyObjectBuilder_CubeGrid>) Reflection.GetValue(sourceBlock, "m_savedProjections");
+            Delegate initFromObjectBuilder = Reflection.GetMethod(typeof(MyProjectorBase), destinationBlock, "InitFromObjectBuilder");
 
             if (projectedGrids == null)
                 return;
 
-            InitFromObjectBuilder.DynamicInvoke(projectedGrids, null);
+            initFromObjectBuilder.DynamicInvoke(projectedGrids, null);
         }
     }
 }
