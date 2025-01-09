0683e2244877accd66d317e4f03406ef11fff2f6
Viktor Ferenczi <viktor@ferenczi.eu>
2025-01-09 12:38:08 +0100

    0.8.0

--- MultigridProjectorApi/Api/IMultigridProjectorApi.cs
+++ MultigridProjectorApi/Api/IMultigridProjectorApi.cs
@@ -7,7 +7,7 @@ namespace MultigridProjector.Api
 {
     public interface IMultigridProjectorApi
     {
-        // Multigrid Projector version: 0.7.9
+        // Multigrid Projector version: 0.8.0
         string Version { get; }
 
         // Returns the number of subgrids in the active projection, returns zero if there is no projection
--- MultigridProjectorApi/Api/MultigridProjectorModShim.cs
+++ MultigridProjectorApi/Api/MultigridProjectorModShim.cs
@@ -44,7 +44,7 @@ namespace MultigridProjector.Api
         private Vector3I projectionOffset;
         private Vector3I projectionRotation;
 
-        public string Version => "0.7.9";
+        public string Version => "0.8.0";
 
         public int GetSubgridCount(long projectorId) => projector.IsProjecting ? 1 : 0;
 
