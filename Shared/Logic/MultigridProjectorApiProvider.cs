6f69095e3601c9dbf840e5c2063fa14521e58e4b
Viktor Ferenczi <viktor@ferenczi.eu>
2026-07-04 21:33:34 +0200

    0.9.4 (#138)
    - Fix #99: Crash on Load Repair Projection
    - Backported template fixes

--- Shared/Logic/MultigridProjectorApiProvider.cs
+++ Shared/Logic/MultigridProjectorApiProvider.cs
@@ -18,7 +18,7 @@ namespace MultigridProjector.Logic
         private static MultigridProjectorApiProvider api;
         public static IMultigridProjectorApi Api => api ?? (api = new MultigridProjectorApiProvider());
 
-        public string Version => "0.9.3";
+        public string Version => "0.9.4";
 
         public int GetSubgridCount(long projectorId)
         {
